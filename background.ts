dfc5d0b55046dc16d862f5237a3423c743674248
Tristen Dillon <tristendillon124@gmail.com>
2024-05-27 11:22:47 -0500

    chore: Remove unused background.ts file and update package.json permissions

--- background.ts
+++
@@ -1,9 +0,0 @@
-
-chrome.webRequest.onCompleted.addListener(
-  function(details)
-  {
-      console.log(details);
-  },
-  {urls: ["https://lm-api-reads.fantasy.espn.com/*", "https://fantasy.espn.com/*",]},
-  ['responseHeaders', 'extraHeaders']
-);
